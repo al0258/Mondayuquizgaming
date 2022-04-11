fc64fad94b66d843add8735098f5276c42ae81f7
Amit Lago <amitlago@Amits-MacBook-Pro.local>
2022-04-11 22:28:37 +0300

    finished game final with server

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express = require('express');
+//Install express server
+const express = require("express");
+const path = require("path");
 
-const path = require('path');   
+const app = express();
 
-const app = express();   
-const PORT = process.env.PORT || 5000
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/quiz-game"));
 
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/quiz-game/" })
+);
 
-
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
-
-// Start the app by listening on the default Heroku port    
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
